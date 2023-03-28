38f7aba4d33f6f70064074bd4258add7d8c39b96
Dennismwendwa <dennismusemb2@.com>
2023-03-28 07:35:56 +0300

    fixed bug in print_binary function

--- functionformat.c
+++ functionformat.c
@@ -184,7 +184,7 @@ int print_binary(va_list types, char buffer[], int flags, int width,
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
 		sum += a[i];
-		if (sum || i == 32)
+		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
 
